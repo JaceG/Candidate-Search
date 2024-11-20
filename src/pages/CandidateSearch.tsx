90fb2715cd00d163632286cd9eb50578a74fbf06
Jace Galloway <jace.galloway@gmail.com>
2024-11-20 14:58:46 -0500

    fixed token error for searching

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -2,6 +2,20 @@ import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
 const CandidateSearch = () => {
+  const [users,setUsers] = useState<Object[]>([])
+  const getUsers = async () => {
+    try {
+      const data = await searchGithub();
+      console.log('apiii',data);
+      // setUsers(data);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  },[]);
   return <h1>CandidateSearch</h1>;
 };
 

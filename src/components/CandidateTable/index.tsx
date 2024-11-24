222fe1fbef7f5bbedde58d40cc13b37f307ba48b
Jace Galloway <jace.galloway@gmail.com>
2024-11-24 14:40:47 -0500

    updated Candidate Table

--- src/components/CandidateTable/index.tsx
+++ src/components/CandidateTable/index.tsx
@@ -10,33 +10,55 @@ const CandidateTable  = ({ candidates, onRejectClicked }:CandidateTableProps) =>
     <tr>
         <th>Image</th>
         <th>Name</th>
+        <th>Username</th>
         <th>Location</th>
         <th>Email</th>
         <th>Company</th>
+        <th>X</th>
+        <th>Website</th>
         <th>Bio</th>
         <th>Reject</th>
     </tr>
       {candidates?.map((candidate: User) => {
         return (
           <tr>
-         <td>
+            <td>
               <img src={candidate.avatar_url}/>
-          </td>
-          <td>src={candidate.name}</td>
-          <td>{candidate.location}</td>
-          <td><a href={'mailto:'+candidate.email}>{candidate.email}</a></td>
-          <td>{candidate.company}</td>
-          <td>{candidate.bio}</td>
-          <td>
-      <button className="btn btn-danger"
-      onClick={() => onRejectClicked(candidate)}
-      >
-              <svg width="35px" height="35px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="btn-svg">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
-      </svg>
-      </button>
-      </td>
-      </tr>
+            </td>
+            <td>{candidate.name || "N/A"}</td>
+            <td>{candidate.login || "N/A"}</td>
+            <td>{candidate.location || "N/A"}</td>
+            <td>{
+              candidate.email ? 
+              <a href={'mailto:'+candidate.email}>{candidate.email}</a>
+              : "N/A"      
+              }
+            </td>
+            <td>{candidate.company || "N/A"}</td>
+            <td> {
+                  candidate.twitter_username ? 
+                  <a href={`https://x.com/${candidate.twitter_username}`} target="_blank" rel="noopener noreferrer">{candidate.twitter_username}</a>
+                  : " N/A"
+                }
+            </td>
+            <td>
+            {
+              candidate.blog ? 
+              <a href={`${candidate.blog}` } target="_blank" rel="noopener noreferrer">{candidate.blog}</a>
+              : " N/A"
+            }
+            </td>
+            <td>{candidate.bio || "N/A"}</td>
+            <td>
+        <button className="btn btn-danger"
+        onClick={() => onRejectClicked(candidate)}
+        >
+                <svg width="35px" height="35px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="btn-svg">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+        </svg>
+        </button>
+        </td>
+        </tr>
         )
       })}
 
